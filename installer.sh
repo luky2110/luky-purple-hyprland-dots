9598003331f031c4bfb7ef2558805944bd738651
luky2110 <kemlo2110@gmail.com>
2026-04-20 18:05:53 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -5,3 +5,6 @@ mv fastfetch/* ~/.config
 mv hypr/* ~/.config
 mv waybar/* ~/.config
 mv wofi/* ~/.config
+
+echo "Restart hyprland to apply everything, have fun!"
+rm -rf ~/luky-purple-hyprland-dots
