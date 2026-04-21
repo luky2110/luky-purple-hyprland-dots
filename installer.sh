b6862cdf465021cf3c936e4cceea4d1597e6f813
luky2110 <kemlo2110@gmail.com>
2026-04-21 16:49:51 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,5 +6,7 @@ mv hypr/* ~/.config
 mv waybar/* ~/.config
 mv wofi/* ~/.config
 
-echo "Restart hyprland to apply everything, have fun!"
+echo "!IMPORTANT!"
+echo "Read the README on github else not everything will work as expected"
+echo "If something did not apply restart Hyprland and have fun!"
 rm -rf ~/luky-purple-hyprland-dots
