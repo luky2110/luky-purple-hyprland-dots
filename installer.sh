46dc091da6d3e05474b44f4d2e751126e9546a78
luky2110 <kemlo2110@gmail.com>
2026-05-06 19:58:06 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,11 +6,11 @@ rm -rf ~/.config/hypr
 rm -rf ~/.config/waybar
 rm -rf ~/.config/wofi
 
-mv alacritty/* ~/.config
-mv fastfetch/* ~/.config
-mv hypr/* ~/.config
-mv waybar/* ~/.config
-mv wofi/* ~/.config
+mv alacritty/ ~/.config
+mv fastfetch/ ~/.config
+mv hypr/ ~/.config
+mv waybar/ ~/.config
+mv wofi/ ~/.config
 
 echo "!IMPORTANT!"
 echo "Read the README on github else not everything will work as expected"
